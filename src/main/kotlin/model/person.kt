788871053f8606249af9a260198cab39683b2968
Forrest <forrestknightjr@gmail.com>
2017-09-08 02:19:23 -0400

    same place again

--- src/main/kotlin/model/person.kt
+++ src/main/kotlin/model/person.kt
@@ -2,38 +2,34 @@ package model
 
 
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
-
 class Model{
 
 fun main(args: Array<String>) {
     var inputs = readLine()
     var data = readLine()?.split(" ")?.map { s -> s.toInt() }
     var swaps = inputs?.split(" ")?.get(1)?.toInt()
-    var sortedMap: SortedMap<Int,Int>
 
     if(data != null && swaps != null) {
-        sortedMap = convertListToSortedListOfMaps(data)//Descending
-        val answer =largestPermutation(data, swaps, sortedMap)
+        val answer =largestPermutation(data, swaps)
         answer.forEach { s -> print("$s ") }
     }
 }
 
-fun largestPermutation(integerList: List<Int>, swaps:Int, sortedMap: SortedMap<Int, Int>): List<Int>{
+fun largestPermutation(integerList: List<Int>, swaps:Int): List<Int>{
     var answerList:List<Int> = integerList.toMutableList()
     var curIndex = 0
     var swapsUsed = swaps
+    val sortedDescriptorList = getSortedListOfNumberDescriptor(integerList)
     while(curIndex < swapsUsed && curIndex < integerList.size){
-//        var swapIndex = getIndexOfLargestValue(sortedMap,0)
-//        if(swapIndex !=curIndex){
-//            answerList = swap(answerList, curIndex , swapIndex)
-//            curIndex+=1
-//        }
-//        else {
-//            curIndex+=1
-//            swapsUsed+=1
-//        }
+        var swapIndex = sortedDescriptorList[curIndex].originIndex
+        if(swapIndex !=curIndex){
+            answerList = swap(answerList, curIndex , swapIndex)
+            curIndex+=1
+        }
+        else {
+            curIndex+=1
+            swapsUsed+=1
+        }
     }
 
     return answerList
@@ -47,36 +43,25 @@ fun largestPermutation(integerList: List<Int>, swaps:Int, sortedMap: SortedMap<I
         newList[ind2] = swap1
         newList[ind1] = swap2
 
-
         return newList
     }
 
-    /**
-     * Returns the index from the original data set of the element we need to swap with
-     */
-    fun getIndexOfLargestValue(sortedMap:SortedMap<Int,Int>, startLookingIndex:Int):Int{
-        var indexToReturn:Int= startLookingIndex
-        sortedMap.asSequence().forEachIndexed { index, entry ->
-            if(index == startLookingIndex){
-                indexToReturn = entry.value.toInt()
-                return@forEachIndexed
-            }
-         }
-        return indexToReturn
+    fun getSortedListOfNumberDescriptor(paramList:List<Int>):List<NumberDescriptor>{
+        var answerList = mutableListOf<NumberDescriptor>()
+        (0 until paramList.size).mapTo(answerList) { NumberDescriptor(it, paramList[it]) }.sort()
+        return answerList
     }
+}
 
-    fun convertListToSortedListOfMaps(paramList:List<Int>):List<Map<Int,Int>>{
-        var sortedList = paramList.sorted().reversed()
-        var listOfMaps:MutableCollection<List>
-       for (i in 0 until sortedList.size){
-           var localMap = HashMap<Int,Int>().put(sortedList[i],i)
-            listOfMaps.add(localMap)
-       }
-        var sortedMap = map.toSortedMap()
-        return sortedMap
+class NumberDescriptor(val originIndex:Int, val value:Int) : Comparable<NumberDescriptor> {
+    override fun compareTo(other: NumberDescriptor): Int {
+        return other.value.compareTo(this.value)
     }
 
 }
+
+
+
 
 
 
--- src/test/kotlin/Testit.kt
+++ src/test/kotlin/Testit.kt
@@ -5,41 +5,13 @@ import org.junit.Test
 
 class TestIt{
 
-    @Test
-    fun testIt(){
-        val testModel = Model()
-//        assertEquals(listOf(5, 4, 3, 2, 1), testModel.largestPermutation(listOf(4, 5, 3, 2, 1), 1))
-
-    }
-
-    @Test
-    fun testTwoSwaps(){
-        val testModel = Model()
-        var testList = listOf(6,9,6,5,4,3,7)
-        var expected2 = listOf(9,7,6,5,4,3,6)
-        var sortedMap = testModel.convertListToSortedListOfMaps(testList)
-
-        assertEquals(expected2, testModel.largestPermutation(testList, 2, sortedMap))
-    }
-
-    @Test
-    fun testConvertListToMap(){
-        val testModel = Model()
-        var testList = listOf(6,9,6,5,4,3,7)
-
-        var sortedMap = testModel.convertListToSortedListOfMaps(testList)
-
-        sortedMap.asSequence().forEachIndexed { index, entry ->
-            println("${index} : ${entry}")
-        }
-    }
 
     @Test
     fun smallData(){
         val testModel = Model()
         var testList = listOf(2,1)
         var expected2 = listOf(2,1)
-//        assertEquals(expected2, testModel.largestPermutation(testList, 1))
+        assertEquals(expected2, testModel.largestPermutation(testList, 1))
     }
 
     @Test
@@ -51,34 +23,14 @@ class TestIt{
     }
 
     @Test
-    fun testGetIndexOfLargestValue(){
-        val testModel = Model()
-        val testList = listOf(6,900000,6,5,4,3,7,900,42,36,11,20,40,6,40000)
-        val expected = 1
-
-        assertEquals(expected, testModel.getIndexOfLargestValue(testModel.convertListToSortedListOfMaps(testList), 0))
-
-    }
-    @Test
-    fun testGetIndexOfLargestValueSmallDataSet(){
-        val testModel = Model()
-        val testList = listOf(6,900000)
-        val expected = 1
-
-        assertEquals(expected, testModel.getIndexOfLargestValue(testModel.convertListToSortedListOfMaps(testList), 0))
-
-    }
-
-    @Test
     fun testAReallyBigOne(){
         val testModel = Model()
         val dataLines = TestIt::class.java.getResource("testInput.txt").readText()
         val expected = TestIt::class.java.getResource("expected.txt").readText().split(" ").map { s->s.toInt() }.toMutableList()
         val swaps = dataLines.split(System.lineSeparator()).get(0).split(" ").get(1).toInt()
-        println("Swaps is $swaps")
         val data = dataLines.split(System.lineSeparator()).get(1).split(" ").map{s->s.toInt()}.toMutableList()
-//
 
+        assertEquals(expected, testModel.largestPermutation(data, swaps))
     }
 
     @Test
@@ -90,6 +42,6 @@ class TestIt{
         println("Swaps is $swaps")
         val data = dataLines.split(System.lineSeparator()).get(1).split(" ").map{s->s.toInt()}.toMutableList()
 
+        assertEquals(expected, testModel.largestPermutation(data,swaps))
     }
-
 }
